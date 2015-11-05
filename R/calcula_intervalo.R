c5b4b3843757859e2edd458cd028f24ca851cbec
felasa <felasa@gmail.com>
2015-11-05 10:15:53 -0600

    revert

--- R/calcula_intervalo.R
+++ R/calcula_intervalo.R
@@ -1,5 +1,6 @@
 calcula_intervalo <- function(evento, t_inicio, t_evento, t_fin, scale=1) {
   time <- ifelse(evento, as.numeric(t_evento - t_inicio),
                  as.numeric(t_fin - t_inicio))
+  time <- replace(time, is.infinite(time), NA)
   time / scale
 }
