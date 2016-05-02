51aa16ceb969870ec2d24677db249ec60ab0c0d6
felasa <felasa@gmail.com>
2016-05-02 15:28:49 -0500

    Funcion para series

--- R/diff0.R
+++ R/diff0.R
@@ -1,3 +1,17 @@
+perc_change <- function(prev, current) {
+  r <- 100*(current - prev) / prev
+  r
+}
+
+change_fun <- function(series, lag=1, fun=perc_change) {
+  n <- length(series)
+  result <- numeric(n-1)
+  for(i in 1:(n-1)) {
+    result[i] <- fun(series[i], series[i+1])
+  }
+  result
+}
+
 diff0 <- function(x, ...) {
   if (!is.vector(x)) stop("Muste be vetor")
   return(c(0,diff(x , ...)))
